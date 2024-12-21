e969cde37c51ea65152c1bba59af818bcf9ddf32
Sandun Pushpika <sandun.pushpika@aizenit.com>
2024-12-21 15:21:08 +0530

    Added basic logging

--- src/main/java/com/task/productmanagement/configuration/ExceptionHandler.java
+++ src/main/java/com/task/productmanagement/configuration/ExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.task.productmanagement.configuration;
 
 import com.task.productmanagement.core.dto.response.ApiResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @ControllerAdvice
 public class ExceptionHandler {
+
+    private static final Logger logger = LogManager.getLogger(ExceptionHandler.class.getName());
 
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse> handleValidationException(MethodArgumentNotValidException exception) {
@@ -23,6 +27,8 @@ public class ExceptionHandler {
 
         String errorMessage = String.join(" and ", messages);
 
+        logger.error(errorMessage);
+
         ApiResponse response = new ApiResponse(errorMessage, HttpStatus.BAD_REQUEST.value(), null);
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
@@ -30,6 +36,8 @@ public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleCommonException(Exception exception) {
+
+        logger.error(exception.getMessage());
 
         String errorMessage = "Error occurred during processing of request";
 
